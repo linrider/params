3e4dd8f0377cc85163e61b63d982de2e3f9d0278
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-11-05 19:16:54 +0200

    initial commit

---
+++ src/main/java/ua/vladyslav/params/controller/ParamController.java
@@ -0,0 +1,53 @@
+package ua.vladyslav.params.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/request")
+public class ParamController {
+    @GetMapping()
+    public String getUserInfo(@RequestParam(name = "name", required = false, defaultValue = "anonime") String name, @RequestParam("age") int age) {
+        System.out.println(String.format("Hello %s, %d", name, age));
+        return "Main";
+
+    }@GetMapping("/{age}")
+    public String getUserInfo1(@RequestParam(name = "name", required = false, defaultValue = "anonime") String name, @PathVariable("age") int age) {
+        System.out.println(String.format("Hello %s, %d", name, age));
+        return "Main";
+    }
+
+    /*Зробити калькулятор він приймає паз веріеблом action(plus,minus,mnozh,dilen), 
+    а також два реквест параметри a та b і виконує цю операцію над ними і результат виводить в консоль.
+    ВИкористовувати switch case */
+    
+    @GetMapping("/calc")
+    public String getCalculator(@RequestParam(name = "arg1") float arg1, 
+                                @RequestParam(name = "arg2") float arg2, 
+                                @RequestParam(name = "action") String action) {
+            System.out.println(switch(action) {
+                case "+" -> arg1 + arg2;
+                case "-" -> arg1 - arg2;
+                case "*" -> arg1 * arg2;
+                case "/" -> arg1 / arg2;
+                default -> "Something went wrong";
+            });
+        return "Main";
+    }
+
+    /*
+     * Зробити обмін валют кажем з якої валюти в яку міняєм і суму передаєм яку міняєм 
+     * (долар злоті гривні) вальта 1 і валюта 2  реквкст параметри, сума це pathvariable
+     */
+
+     @GetMapping("/exchange")
+     public String getCurrencyExchange(@RequestParam String param) {
+         return new String();
+     }
+     
+    
+}
